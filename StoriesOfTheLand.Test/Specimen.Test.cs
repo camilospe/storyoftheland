bfc42e2017fb0552877f6caa2c8dd70c5af96062
Jett Bolen <bolen8781@saskpolytech.ca>
2023-10-12 14:54:12 -0600

    Trying to figure out what Ernesto wants for pre-code

--- StoriesOfTheLand.Test/Specimen.Test.cs
+++ StoriesOfTheLand.Test/Specimen.Test.cs
@@ -4,12 +4,13 @@ namespace StoriesOfTheLand.Test
 {
     public class Tests
     {
+        private Specimen specimen;
 
 
         [SetUp]
         public void Setup()
         {
-            
+            specimen = new Specimen();
         }
 
         [Test]
@@ -24,19 +25,29 @@ namespace StoriesOfTheLand.Test
         [Test]
         public void testThatCulturalSignificanceCanBeCorrectlyRetrieved()
         {
-            
+            // Adds a cultural significance which is valid
+            specimen.culturalSignificance = "This is the cultural significance for the sage specimen";
 
+            var errors = ValidationHelper.Validate(specimen);
+            Assert.IsEmpty(errors); // Tests that there are no errors
         }
 
         [Test]
-        public void testThatCulturalSignificanceDisplaysAMessageIfNothingIsEntered()
+        public void testThatCulturalSignificanceMustBeAtLeast1Character()
         {
-            
+            // Adds a cultural significance which is an empty string
+            specimen.culturalSignificance = "";
+
+            var errors = ValidationHelper.Validate(specimen);
+
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual("Cultural Significance must have between 1 and 3500 characters", errors[0].ErrorMessage);
         }
 
         [Test]
-        public void testThatCulturalSignificanceDoesNotAllowInvalidInputs()
+        public void testThatCulturalSignificanceCannotExceed3500Characters()
         {
-            
+
+        }
     }
-}
+}   
--- StoriesOfTheLand/Models/Speciman.cs
+++ StoriesOfTheLand/Models/Speciman.cs
@@ -2,9 +2,16 @@
 
 namespace StorisOfTheLand.Models
 {
-    public class Specimen
+    public class Specimen : IValidatableObject
     {
-        [Key]
-        public int SpecimenID { get; set; }
+
+        [Required(ErrorMessage = "Cultural Significance is required")]
+        [StringLength(3500, MinimumLength = 1, ErrorMessage = "Cultural Significance must have between 1 and 3500 characters")]
+        public string culturalSignificance { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
