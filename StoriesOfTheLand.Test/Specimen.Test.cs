036ee35a8ea0e0d5c8b5a0f950e6b2c3d639daf4
Moleman114 <jettbolen2@gmail.com>
2023-10-12 13:51:19 -0600

    Erased Tests

--- StoriesOfTheLand.Test/Specimen.Test.cs
+++ StoriesOfTheLand.Test/Specimen.Test.cs
@@ -9,7 +9,7 @@ namespace StoriesOfTheLand.Test
         [SetUp]
         public void Setup()
         {
-            SpecimenController testSpecController = new SpecimenController(); // has to have fixture database context passed in for testing
+            
         }
 
         [Test]
@@ -24,73 +24,19 @@ namespace StoriesOfTheLand.Test
         [Test]
         public void testThatCulturalSignificanceCanBeCorrectlyRetrieved()
         {
-            // Test will create a new Specimen object with the name "Sage" and a cultural significance
-            // It will then add the Specimen to the test SpecimenController
-
-            Specimen testSpecimen = new Specimen();
-            testSpecimen.name = "Lily";
-            testSpecimen.cultural_significance = "This is the cultural significance for the Lily specimen";
-            testSpecController.addSpecimen(testSpecimen);
-
-            // Test will then retrieve the Specimen with the name "Sage" from the SpecimenController
-            // It will assert that the Specimen exists, it's name is "Sage", and it's Cultural Significance matches what was input 
-
-            Specimen readSpecimen = testSpecController.getSpecimen("Lily");
-            Assert.IsNotNull(readSpecimen);
-            Assert.AreEqual(readSpecimen.name, "Lily");
-
-            Assert.AreEqual(readSpecimen.cultural_significance, "This is the cultural significance for the Lily specimen");
-
-            // Result: Returns the Sage specimen's cultural significance 
+            
 
         }
 
         [Test]
         public void testThatCulturalSignificanceDisplaysAMessageIfNothingIsEntered()
         {
-            // Creates a new Specimen object with the name "Grass" and no cultural significance
-            // Adds the Specimen to the test SpecimenController
-
-            Specimen testSpecimen = new Specimen();
-            testSpecimen.name = "Grass";
-            testSpecController.addSpecimen(testSpecimen);
-
-            // Test will then retrieve the Specimen with the name "Grass:
-            // It will assert that the Specimen exists, it's name is grass, and it's cultural significance matches the default error message
-
-            Specimen readSpecimen = testSpecController.getSpecimen("Grass");
-            Assert.IsNotNull(readSpecimen);
-            Assert.AreEqual(readSpecimen.name, "Grass");
-
-            Assert.AreEqual(readSpecimen.cultural_significance, "Cultural Significance has not been entered for this specimen");
-
-            // Result: Returns an error message reading "Cultural Significance has not been entered for this specimen". 
+            
         }
 
         [Test]
         public void testThatCulturalSignificanceDoesNotAllowInvalidInputs()
         {
-            // Creates a new Specimen object named "Lavender" and a cultural significance that is longer than the allowed character limit
-            // Adds it to the SpecimenController
-
-            Specimen testSpecimen = new Specimen();
-            testSpecimen.name = "Lavender";
-
-            string sCult = new string('a', 3600);
-
-            testSpecimen.cultural_significance = sCult;
-            testSpecController.addSpecimen(testSpecimen);
-
-            // Retrieves the specimen with the name "Lavender"
-            // Asserts that it exists, has the right name, and matches the error message for too many characters
-
-            Specimen readSpecimen = testSpecController.getSpecimen("Lavender");
-            Assert.IsNotNull(readSpecimen);
-            Assert.AreEqual(readSpecimen.name, "Lavender");
-
-            Assert.AreEqual(readSpecimen.cultural_significance, "Cultural Significance has not been entered for this specimen");
-
-            // Result: Throws an exception reading "Cultural Significance exceeds allowed character limit (3500 Characters)"
-        }
+            
     }
 }
