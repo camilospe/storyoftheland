d9281f4d6b2e96ece515253980fcd203c90d3ff0
Jett Bolen <bolen8781@saskpolytech.ca>
2023-10-17 13:49:41 -0600

    Added SpecimensController scaffolded controller and re-inserted bootstrap styles

---
+++ StoriesOfTheLand/Controllers/SpecimensController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StoriesOfTheLand.Data;
+using StorisOfTheLand.Models;
+
+namespace StoriesOfTheLand.Controllers
+{
+    public class SpecimensController : Controller
+    {
+        private readonly StoriesOfTheLandContext _context;
+
+        public SpecimensController(StoriesOfTheLandContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Specimens/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Specimen == null)
+            {
+                return NotFound();
+            }
+
+            var specimen = await _context.Specimen
+                .FirstOrDefaultAsync(m => m.SpecimenID == id);
+            if (specimen == null)
+            {
+                return NotFound();
+            }
+
+            return View(specimen);
+        }
+
+    }
+}
