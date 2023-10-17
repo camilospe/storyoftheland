76bbfda601b5daa0be03028eafa82309c761aa32
Ethan Trebish <trebish3190@Saskpolytech.ca>
2023-10-17 14:51:45 -0600

    Followed microsft instructions and created a new database, worked with other members in setting up the Database

---
+++ StoriesOfTheLand/Models/SeedData.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StoriesOfTheLand.Models;
+using StoriesOfTheLand.Data;
+using System;
+using System.Linq;
+using StorisOfTheLand.Models;
+
+namespace StoriesOfTheLand.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new StoriesOfTheLandContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<StoriesOfTheLandContext>>()))
+        {
+            // Look for any Specimens.
+            if (context.Specimen.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Specimen.AddRange(
+                new Specimen
+                {
+                    LatinName = "Latin Name 4",
+
+                },
+                new Specimen
+                {
+                    LatinName = "Latin Name 2 ",
+
+                },
+                new Specimen
+                {
+                    LatinName = "Latin Name 3",
+      
+                },
+                new Specimen
+                {
+                    LatinName = "Latin Name 4",
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
