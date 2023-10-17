097b2ce08b661aa35017e82263ac64f6cacba840
Jett Bolen <bolen8781@saskpolytech.ca>
2023-10-17 13:25:31 -0600

    Fixed issues with pre-code. (Added upper bounds test and null test, re-added specimenID to entity, changed property name to Pascal Case)

--- StoriesOfTheLand.Test/Specimen.Test.cs
+++ StoriesOfTheLand.Test/Specimen.Test.cs
@@ -10,7 +10,7 @@ namespace StoriesOfTheLand.Test
         [SetUp]
         public void Setup()
         {
-            specimen = new Specimen();
+            specimen = new Specimen() { CulturalSignificance= "This is the cultural significance for the sage specimen" };
         }
 
         [Test]
@@ -26,7 +26,7 @@ namespace StoriesOfTheLand.Test
         public void testThatCulturalSignificanceCanBeCorrectlyRetrieved()
         {
             // Adds a cultural significance which is valid
-            specimen.culturalSignificance = "This is the cultural significance for the sage specimen";
+            specimen.CulturalSignificance = "This is the cultural significance for the sage specimen";
 
             var errors = ValidationHelper.Validate(specimen);
             Assert.IsEmpty(errors); // Tests that there are no errors
@@ -36,7 +36,7 @@ namespace StoriesOfTheLand.Test
         public void testThatCulturalSignificanceMustBeAtLeast1Character()
         {
             // Adds a cultural significance which is an empty string
-            specimen.culturalSignificance = "";
+            specimen.CulturalSignificance = "";
 
             var errors = ValidationHelper.Validate(specimen);
 
@@ -45,16 +45,40 @@ namespace StoriesOfTheLand.Test
         }
 
         [Test]
+        public void testThatCulturalSignificanceMustBeSet()
+        {
+            // Sets cultural significance to null
+            specimen.CulturalSignificance = null;
+
+            var errors = ValidationHelper.Validate(specimen);
+
+            Assert.AreEqual(1, errors.Count); // Tests that there is only one error
+            Assert.AreEqual("Cultural Significance is required", errors[0].ErrorMessage); // Tests that there is a StringLength error
+        }
+
+        [Test]
         public void testThatCulturalSignificanceCannotExceed3500Characters()
         {
             // Adds a cultural significance which is string of 3501 characters
             string testString = new string('a', 3501);
-            specimen.culturalSignificance = testString;
+            specimen.CulturalSignificance = testString;
 
             var errors = ValidationHelper.Validate(specimen);
 
             Assert.AreEqual(1, errors.Count); // Tests that there is only one error
             Assert.AreEqual("Cultural Significance must have between 1 and 3500 characters", errors[0].ErrorMessage); // Tests that there is a StringLength error
+        }
+
+        [Test]
+        public void testThatCulturalSignificanceCanGoUpTo3500Characters()
+        {
+            // Adds a cultural significance which is string of 3500 characters
+            string testString = new string('a', 3500);
+            specimen.CulturalSignificance = testString;
+
+            var errors = ValidationHelper.Validate(specimen);
+
+            Assert.IsEmpty(errors); // Tests that there are no errors
         }
     }
 }   
--- StoriesOfTheLand/Models/Speciman.cs
+++ StoriesOfTheLand/Models/Speciman.cs
@@ -4,10 +4,12 @@ namespace StorisOfTheLand.Models
 {
     public class Specimen : IValidatableObject
     {
+        [Key]
+        public int SpecimenID { get; set; }
 
         [Required(ErrorMessage = "Cultural Significance is required")]
         [StringLength(3500, MinimumLength = 1, ErrorMessage = "Cultural Significance must have between 1 and 3500 characters")]
-        public string culturalSignificance { get; set; }
+        public string CulturalSignificance { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
