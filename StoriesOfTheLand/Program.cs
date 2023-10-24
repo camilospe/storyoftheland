f3844026c0022d95d0e67b1ce9ada6f4229c52b6
Sean Lusher <lusher9578@saskpolytech.ca>
2023-10-24 13:32:59 -0600

    merged master into story24v2

--- StoriesOfTheLand/Program.cs
+++ StoriesOfTheLand/Program.cs
@@ -2,20 +2,27 @@
 using Microsoft.Extensions.DependencyInjection;
 using StoriesOfTheLand.Data;
 using StoriesOfTheLand.Models;
+<<<<<<< HEAD
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<StoriesOfTheLandContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("StoriesOfTheLandContext") ?? throw new InvalidOperationException("Connection string 'StoriesOfTheLandContext' not found."));
+    
+});
+
+
+=======
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<StoriesOfTheLandContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("StoriesOfTheLandContext") ?? throw new InvalidOperationException("Connection string 'StoriesOfTheLandContext' not found.")));
+>>>>>>> master
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-
-    SeedData.Initialize(services);
-}
+<<<<<<< HEAD
 
 //https://stackoverflow.com/questions/71461296/how-do-you-do-database-ensurecreated-in-aspnet-core-web-application-using-net
 
@@ -27,13 +34,23 @@ using (var scope = app.Services.CreateScope())
 
     var Services = scope.ServiceProvider;
     SeedData.Initialize(Services);
+=======
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+>>>>>>> master
 }
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+<<<<<<< HEAD
+=======
 
+>>>>>>> master
 }
 app.UseStaticFiles();
 
@@ -43,8 +60,12 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
+<<<<<<< HEAD
     pattern: "{controller=Home}/{action=Index}/{id?}"
     );
+=======
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
+>>>>>>> master
 
 app.Run();
