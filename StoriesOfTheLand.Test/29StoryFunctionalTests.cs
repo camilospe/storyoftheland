31d85ad282bafb857105d6fe5fa2b7a02bab8c43
Morgan Hamata <hamata9905@saskpolytech.ca>
2023-11-16 13:54:05 -0600

    creating mock db for testing, attmpting mock repo

--- StoriesOfTheLand.Test/29StoryFunctionalTests.cs
+++ StoriesOfTheLand.Test/29StoryFunctionalTests.cs
@@ -8,6 +8,11 @@ using System.Threading.Tasks;
 using Xunit;
 using StoriesOfTheLand.Data;
 using StoriesOfTheLand;
+using Microsoft.AspNetCore.Http.HttpResults
+using StoriesOfTheLand.Test.Utilities;
+using StoriesOfTheLand.Controllers;
+using StoriesOfTheLand.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace StoriesOfTheLand.Test
 {
@@ -19,7 +24,41 @@ namespace StoriesOfTheLand.Test
         {
             _context = context;
         }
-        
+
+        [Fact]
+        public async Task GetIndexReturnsSponsors()
+        {
+            // Arrange
+            var mockRepo = new MockDb<StoriesOfTheLandContext>();
+            mockRepo.Setup(repo => repo.ListAsync())
+                .ReturnsAsync(GetTestSessions());
+
+            await using var context = new MockDb().CreateDbContext();
+            var controller = new HomeController(mockRepo.Object);
+
+            context.Add(new Sponsor
+            { SponsorID = 1,
+              SponsorName = "Test1",
+              SponsorURL = "Test1.ca"
+            });
+
+            context.Add(new Sponsor
+            {
+                SponsorID = 1,
+                SponsorName = "Test2",
+                SponsorURL = "Test3.ca"
+            });
+
+            // Act
+            var result = await HomeController.Index();
+
+            //Assert
+            var viewResult = Assert.IsInstanceOf<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(
+                viewResult.ViewData.Model);
+            Assert.AreEqual(2, model.Count());
+        }
+
 
 
 
